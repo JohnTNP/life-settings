cdb756a0e76d0f4edb1e9c7c45732c2391309b72
johntnp <tanaphon86@gmail.com>
2024-09-04 22:51:56 +0700

    ✨feat: mock dynamic forms and ui/sonner

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from '@/providers/theme-provider'
 import { Home } from '@/components/home'
 import { AppStateProvider } from './providers/app-state-provider'
 import Navbar from './components/navbar'
+import { Toaster } from '@/components/ui/sonner'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <div className='w-full max-w-7xl gap-2 mx-auto items-center flex flex-col h-screen p-4 font-sans'>
             <Navbar />
             <Home />
+            <Toaster position='top-right' duration={2000} />
         </div>
       </AppStateProvider>
     </ThemeProvider>
